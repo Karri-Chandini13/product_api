6a7575d688e21bcce779bfca774bfa0a129fcfa9
Karri-Chandini13 <chandinikarri75@gmail.com>
2026-08-13 16:37:09 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:123456@localhost/product_db"
+DATABASE_URL = "mysql+pymysql://karri1309:Chandini@99@karri1309-chandinikarri99.l.aivencloud.com:17224/product_db"
 
 engine = create_engine(DATABASE_URL)
 
